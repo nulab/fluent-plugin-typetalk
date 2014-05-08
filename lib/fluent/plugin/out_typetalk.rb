d4c4d75f4d094ea073a69f49af0f95fff4778b4d
someda <someda@isenshi.com>
2014-05-09 02:23:27 +0900

    implement method bodies

--- lib/fluent/plugin/out_typetalk.rb
+++ lib/fluent/plugin/out_typetalk.rb
@@ -2,23 +2,36 @@ module Fluent
   class TypetalkOutput < Fluent::BufferedOutput
     Fluent::Plugin.register_output('typetalk', self)
 
+    config_param :client_id, :string
+    config_param :client_secret, :string
+    config_param :topic_id, :integer
+    config_param :template, :string, :default => "<%= tag %> at <%= Time.at(time).localtime %>\n<%= record.to_json %>"
+    config_param :flush_interval, :time, :default => 1
+
+    attr_reader :typetalk
+
     def initialize
       super
-      # require 'something'
+      require 'net/https'
+      require 'uri'
+      require 'json'
+      require 'erb'
     end
 
     def configure(conf)
       super
+
+      @typetalk = Typetalk.new(conf['client_id'], conf['client_secret'])
+      @topic_id = conf['topic_id']
+      @template = conf['template']      
     end
 
     def start
       super
-      # init
     end
 
     def shutdown
       super
-      # destroy
     end
 
     def format(tag, time, record)
@@ -26,7 +39,46 @@ module Fluent
     end
 
     def write(chunk)
-      # write records
+      chunk.msgpack_each do |(tag,time,record)|
+        begin
+          send_message(tag, time, record)
+        rescue => e
+          $log.error("Typetalk Error:", :error_class => e.class, :error => e.message)
+        end
+      end
     end
+
+    def send_message(tag, time, record)
+      message = ERB.new(@template).result(binding)
+      @typetalk.post(@topic_id, message)
+    end
+
   end
+
+  class Typetalk
+
+    def initialize(client_id, client_secret)
+      @client_id = client_id
+      @client_secret = client_secret
+    end
+
+    def post(topic_id, message)
+      http = Net::HTTP.new('typetalk.in', 443)
+      http.use_ssl = true
+      res = http.post(
+        '/oauth2/access_token',
+        "client_id=#{client_id}&client_secret=#{client_secret}&grant_type=client_credentials&scope=topic.post"
+      )
+      json = JSON.parse(res.body)
+      access_token = json['access_token']
+
+      req = Net::HTTP::Post.new("/api/v1/topics/#{topic_id}")
+      req['Authorization'] = "Bearer #{access_token}"
+      req.set_form_data({:message=>message})
+      http.request(req)
+      
+    end
+
+  end
+
 end
--- test/plugin/test_out_typetalk.rb
+++ test/plugin/test_out_typetalk.rb
@@ -7,6 +7,10 @@ class TypetalkOutputTest < Test::Unit::TestCase
   end
 
   CONFIG = %[
+    type typetalk
+    client_id 123456
+    client_secret secret
+    topic_id 1
   ]
 
   def create_driver(conf = CONFIG, tag = 'test')
@@ -14,10 +18,13 @@ class TypetalkOutputTest < Test::Unit::TestCase
   end
 
   def test_configure
+    d = create_driver()
+    assert_equal d.instance.typetalk.instance_variable_get(:@client_id), '123456'
+    assert_equal d.instance.typetalk.instance_variable_get(:@client_secret), 'secret'
   end
 
-  def test_write
-    d = create_driver
-  end
+#  def test_write
+#    d = create_driver
+#  end
 
 end
